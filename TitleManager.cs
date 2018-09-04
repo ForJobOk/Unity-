92d5bdaf82d75930537dbb9e1174580e00de1623
ForJobOk <41860455+ForJobOk@users.noreply.github.com>
2018-09-04 18:34:59 +0900

    Add files via upload
    完成！

---
+++ TitleManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class TitleManager : MonoBehaviour {
+    
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+    public void PushStartButton()
+    {
+        SceneManager.LoadScene("StageSelectScene");
+    }
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
