3ffabe4599bcdb457ca0bc1421ecfe458fa5a221
ForJobOk <41860455+ForJobOk@users.noreply.github.com>
2018-09-04 10:30:03 +0900

    Add files via upload
    Unity　物理パズルゲーム　途中経過

---
+++ BallManager.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallManager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    //ボールがCollisionに衝突
+    void OnCollisionEnter2D(Collision2D coll)
+    {
+        if(coll.gameObject.tag == "OutArea")
+        {
+            //ゲームマネージャーを取得
+            GameObject gameManager = GameObject.Find("GameManager");
+
+            //リトライ
+            gameManager.GetComponent<GameManager>().PushRetryButton();
+        }
+    }
+
+    //ボールが何かのトリガーに衝突
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.gameObject.tag == "ClearArea")
+        {
+            //クリアエリアに突入
+            GameObject gameManager = GameObject.Find("GameManager"); //GameManagerのゲームオブジェクトを取得し変数に記憶しておく
+            gameManager.GetComponent<GameManager>().StageClear();  　//記憶したゲームオブジェクトの中からStageClearメソッドを取得
+
+        }
+    }
+}
---
+++ GameManager.cs
@@ -0,0 +1,98 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour {
+
+    public int StageNo; 　　　　　　　　//ステージナンバー
+    public bool isBallMoving; 　　　　　//ボール移動中か否か
+    public GameObject ballPrefab; 　　　//ボールプレハブ
+    public GameObject ball; 　　　　　　//ボールオブジェクト
+
+    public GameObject goButton; 　　　　//ボタン：ゲーム開始
+    public GameObject retryButton;     //ボタン：リトライ
+    public GameObject clearText;       //テキスト:クリア
+
+    public AudioClip clearSE;          //効果音:クリア
+    private AudioSource audioSource;    //オーディオソース
+
+    // Use this for initialization
+    void Start() {
+        Rigidbody2D rd = ball.GetComponent<Rigidbody2D>();
+        rd.isKinematic = true;
+        retryButton.SetActive(false);  //リトライボタンを非表示
+        isBallMoving = false;          //ボールは「移動中ではない」
+
+        //オーディオソースを取得
+        audioSource = gameObject.GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+    }
+    public void PushGoButton()
+    {
+        Gravity(false);       //重力を有効化
+
+        retryButton.SetActive(true); //リトライボタンを表示
+        goButton.SetActive(false);　 //Goボタンを非表示
+        isBallMoving = true;　　　　 //ボールは「移動中」
+    }
+    //リトライボタンを押す
+    public void PushRetryButton()
+    {
+        Destroy(ball);             　//ボールオブジェクトを削除
+
+        //プレハブから新しいボールオブジェクト作成
+        ball = (GameObject)Instantiate(ballPrefab);  //Instantiateは引数に指定したオリジナルのオブジェクトのクローンを返す
+                                                     //変数に代入するときは(GameObject)をつけてキャストする
+
+        Gravity(true); 　　　　　　　　//重力を無効化
+
+        retryButton.SetActive(false);　//リトライボタンを非表示
+        goButton.SetActive(true);　　　//GOボタンを表示
+        isBallMoving = false;　　　　　//ボールは「移動中ではない」
+    }
+
+
+    //戻るボタン
+    public void PushBackButton()
+    {
+        GobackStageSelect(); 
+    }
+
+
+    //ボールの重力を有効(false)、無効(true)に切り替える
+    void Gravity(bool gravity)
+    {
+        Rigidbody2D rd = ball.GetComponent<Rigidbody2D>();
+        rd.isKinematic = (gravity);  //isKinematicは物理エンジンを有効、無効に切り替える(bool型で有効はfalse)
+    }
+
+    //ステージクリア処理
+    public void StageClear()
+    {
+        audioSource.PlayOneShot(clearSE); //クリア音再生
+
+        //セーブデータ更新
+        if(PlayerPrefs.GetInt("CLEAR",0)< StageNo)
+        {
+            //セーブされているステージナンバーより今のステージナンバーの方が大きければ
+            PlayerPrefs.SetInt("CLEAR", StageNo); //ステージナンバーを記録
+        }
+
+        clearText.SetActive(true);        //クリア表示
+        retryButton.SetActive(false);     //リトライボタン非表示
+
+
+        //3秒後に自動的にステージセレクト画面へ
+        Invoke("GobackStageSelect", 3.0f);
+    }
+    //移動処理
+    void GobackStageSelect()  //Invokeで指定する為にメソッド作成
+    {
+        SceneManager.LoadScene("StageselectScene");
+    }
+}
